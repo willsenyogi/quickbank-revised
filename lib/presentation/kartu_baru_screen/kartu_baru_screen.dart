926236667e16793d70fa8affdfd720ed5f1bc05d
TanjayaJason <tanjaya.jason01@gmail.com>
2023-10-07 09:38:54 +0700

    benerin route kartu baru ke mainscreen

--- lib/presentation/kartu_baru_screen/kartu_baru_screen.dart
+++ lib/presentation/kartu_baru_screen/kartu_baru_screen.dart
@@ -142,7 +142,7 @@ class KartuBaruScreen extends StatelessWidget {
   }
 
   onTapBack(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.homepageDoneScreen);
+    Navigator.pushNamed(context, AppRoutes.mainScreen);
   }
 
   onTapKuning(BuildContext context) {
