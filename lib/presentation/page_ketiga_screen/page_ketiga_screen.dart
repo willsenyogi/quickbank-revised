30022f66748ca2c9ec97893afc4014bcb326ca53
seanhw22 <119191741+seanhw22@users.noreply.github.com>
2023-10-08 17:56:36 +0700

    Fixed moving background in 2 pages
    di page_ketiga_screen.dart sama sign_in_done_screen.dart

--- lib/presentation/page_ketiga_screen/page_ketiga_screen.dart
+++ lib/presentation/page_ketiga_screen/page_ketiga_screen.dart
@@ -14,6 +14,7 @@ class PageKetigaScreen extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         extendBody: true,
         extendBodyBehindAppBar: true,
         body: Container(
--- lib/presentation/sign_in_done_screen/sign_in_done_screen.dart
+++ lib/presentation/sign_in_done_screen/sign_in_done_screen.dart
@@ -31,7 +31,7 @@ class _SignInDoneScreenState extends State<SignInDoneScreen> {
       child: Scaffold(
         extendBody: true,
         extendBodyBehindAppBar: true,
-        resizeToAvoidBottomInset: true,
+        resizeToAvoidBottomInset: false,
         appBar: CustomAppBar(
           height: 90.v,
           leadingWidth: 52.h,
